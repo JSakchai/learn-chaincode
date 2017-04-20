9f5a4813500e00e9b00f421565aefe9b48f71aa1
JSakchai <kakaongo@gmail.com>
2017-04-20 12:23:03 +0700

    Complete my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,7 +42,10 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-
+	err := stub.PutState("hello world" , []byte(args[0]))
+	if err != nil {
+		return  nil,err
+	}
 	return nil, nil
 }
 
@@ -53,6 +56,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else  if function == "write" {
+		return  t.write(stub,args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
@@ -71,4 +76,34 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+// write is put data to block
+func (t *SimpleChaincode) write(stup shim.ChaincodeStubInterface, args []string) ([]byte ,error){
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+	if len(args) != 2 {
+		return  nil, errors.New("inconnect number of argument. Expecting 2. name of the key and value to set")
+	}
+	key = args[0]
+	value =args[1]
+	err = stup.PutState(key, []byte(value))
+	if err != nil {
+		return  nil, err
+	}
+	return nil ,nil
+}
+func (t *SimpleChaincode) read(stup shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+	key = args[0]
+	valAsbytes, err := stup.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil,errors.New(jsonResp)
+	}
+	return  valAsbytes , nil
 }
